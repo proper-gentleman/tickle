3df421665a77724c70fffe542dcd655865f7cc92
GitHub Action <action@github.com>
2024-02-08 13:49:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/propergentleman/tickle/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/propergentleman/tickle/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.propergentleman.tickle.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
