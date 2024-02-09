3313d9cdc5d2ecb3cde4f361a5c68a6e2140671c
Andy Alexa <andy.alexa@openbet.com>
2024-02-09 13:53:31 +0200

    working basic highlighting

---
+++ src/main/java/com/propergentleman/tickle/TclSyntaxHighlighterFactory.java
@@ -0,0 +1,15 @@
+package com.propergentleman.tickle;
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+public class TclSyntaxHighlighterFactory extends SyntaxHighlighterFactory {
+    @NotNull
+    @Override
+    public SyntaxHighlighter getSyntaxHighlighter(Project project, VirtualFile virtualFile) {
+        return new TclSyntaxHighlighter();
+    }
+}
